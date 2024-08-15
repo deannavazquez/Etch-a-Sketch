a2325f9495da15cea675f789d5fe93dea65aff92
deannavazquez <deannavazquez5@gmail.com>
2024-08-15 12:31:18 -0500

    create 16 boxes in a container using flexbox

---
+++ javascript.js
@@ -0,0 +1,55 @@
+const container = document.querySelector("#container");
+
+const grid1 = document.createElement("div");
+grid1.classList.add("grid");
+
+const grid2 = document.createElement("div");
+grid2.classList.add("grid");
+
+const grid3 = document.createElement("div");
+grid3.classList.add("grid");
+
+const grid4 = document.createElement("div");
+grid4.classList.add("grid");
+
+const grid5 = document.createElement("div");
+grid5.classList.add("grid");
+
+const grid6 = document.createElement("div");
+grid6.classList.add("grid");
+
+const grid7 = document.createElement("div");
+grid7.classList.add("grid");
+
+const grid8 = document.createElement("div");
+grid8.classList.add("grid");
+
+const grid9 = document.createElement("div");
+grid9.classList.add("grid");
+
+const grid10 = document.createElement("div");
+grid10.classList.add("grid");
+
+const grid11 = document.createElement("div");
+grid11.classList.add("grid");
+
+const grid12 = document.createElement("div");
+grid12.classList.add("grid");
+
+const grid13 = document.createElement("div");
+grid13.classList.add("grid");
+
+const grid14 = document.createElement("div");
+grid14.classList.add("grid");
+
+const grid15 = document.createElement("div");
+grid15.classList.add("grid");
+
+const grid16 = document.createElement("div");
+grid16.classList.add("grid");
+
+
+
+container.append(grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8, grid9, grid10, grid11, grid12, grid13, grid14, grid15, grid16);
+
+
