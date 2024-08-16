55a11ba06e778030df544c10e54679c68a92b637
deannavazquez <deannavazquez5@gmail.com>
2024-08-16 16:37:20 -0500

    Add mouseenter event so the grid divs change color when the mouse passes over them

--- javascript.js
+++ javascript.js
@@ -1,7 +1,17 @@
+const gridItems = [];
+
 const container = document.querySelector("#container");
 
 for (let i = 1; i < 257; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid")
-    container.appendChild(grid);
-    }
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid");
+    container.appendChild(gridItem);
+    gridItems.push(gridItem); // to use globally if needed
+
+    // Attach the click event listener to each grid item
+    gridItem.addEventListener('mouseenter', () => {
+        gridItem.style.backgroundColor = 'black';
+    });
+}
+
+
