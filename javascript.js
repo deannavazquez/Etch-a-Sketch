5d84fcefe9a0de3e3dab9eda372b8d3da712ec45
deannavazquez <deannavazquez5@gmail.com>
2024-08-23 09:18:06 -0500

    Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before so that you’ve got a new sketch pad.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,33 @@
-const gridItems = [];
-
 const container = document.querySelector("#container");
 
-for (let i = 1; i < 257; i++) {
+for (let i = 1; i < (16 * 16) +1; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid");
     container.appendChild(gridItem);
-    gridItems.push(gridItem); // to use globally if needed
 
     // Attach the click event listener to each grid item
-    gridItem.addEventListener('mouseenter', () => {
-        gridItem.style.backgroundColor = 'black';
+    gridItem.addEventListener('mouseout', () => {
+        gridItem.style.backgroundColor = '#FF5161';
     });
 }
 
+function resetGrid() {
+  
+  let input = Number(window.prompt ("How many squares per side for new sketch?"));
+  container.innerHTML = '';
+  for (let i = 1; i < (input * input) +1; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid");
+    container.appendChild(gridItem);
+
+    // Attach the click event listener to each grid item
+    gridItem.addEventListener('mouseout', () => {
+        gridItem.style.backgroundColor = '#FF5161';
+    });
+} }
+  
+  let btn = document.querySelector(".reset");
+  btn.onclick = resetGrid;
+
+  
 
