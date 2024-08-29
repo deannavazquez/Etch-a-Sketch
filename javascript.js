98a81405003c8e4e3fa04a32d8459b6aeb41c888
deannavazquez <deannavazquez5@gmail.com>
2024-08-29 15:26:33 -0500

    Add event listeners for grid drawing and color change
    - Implement `mousedown` listener to activate drawing mode and set a random color.
    - Implement `mouseup` listener to end drawing mode and set a random color.
    - Add color randomization on grid item interaction.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+let draw = false;
 
 function createGrid(squaresPerSide){
     container.innerHTML = ""; // Clears existing grid
@@ -10,15 +11,30 @@ function createGrid(squaresPerSide){
         container.appendChild(gridItem);
         
       let color = ["#FF0000", "#EF00FF", "#1100FF", "#00F7FF", "#00FF11"];  
-        gridItem.addEventListener('mouseenter', () => {
-        gridItem.style.backgroundColor = color[(Math.floor(Math.random() * color.length))];
-      });
+
+      gridItem.addEventListener('mousedown', () => { /// Starts drawing mode and sets a random color when the mouse is pressed.
+        draw = true;
+      gridItem.style.backgroundColor = color[(Math.floor(Math.random() * color.length))];
+    });
+
+      gridItem.addEventListener('mouseenter', () => { // Activates drawing mode and changes the grid item's color to a random one when mouse is pressed.
+        if (draw === true) {
+      gridItem.style.backgroundColor = color[(Math.floor(Math.random() * color.length))];
+    }
+  });
+
+    gridItem.addEventListener('mouseup', () => { // Ends drawing mode and sets a random color when the mouse is released.
+      draw = false;
+    gridItem.style.backgroundColor = color[(Math.floor(Math.random() * color.length))];
+});
+
+
+
         
       let clearBtn = document.querySelector(".clear"); //button to clear grid coloring
         clearBtn.addEventListener('click', () => {
         gridItem.style.backgroundColor = "#FFFFFF";
-      })
-
+      });
   }
 }
 
@@ -26,12 +42,10 @@ function sizeGrid() {
   let input = Number(window.prompt ("How many squares per side for new sketch?"));
   if (input > 0 && input <= 100 ) {
     createGrid(input)
-} else {
-        alert("Please enter a number between 1 and 100");
-    }
+  } else {
+    alert("Please enter a number between 1 and 100");
+  }
 }
-
-
 
 createGrid(16);
   
